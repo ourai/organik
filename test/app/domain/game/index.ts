718f1ddb96bab2fa53895e280a3b1efaa6bb9e49
Ourai Lin <ourairyu@gmail.com>
2021-07-11 11:00:16 +0800

    fix: remove dependents of `common` module

--- test/app/domain/game/index.ts
+++ test/app/domain/game/index.ts
@@ -3,12 +3,4 @@ import { ModuleDescriptor } from 'handie-vue';
 import { MODULE_NAME } from './helper';
 import * as views from './views';
 
-export default {
-  name: MODULE_NAME,
-  views,
-  imports: ['common.widgets.TableView', 'common.widgets.DetailView'],
-  components: {
-    TableView: 'common.widgets.TableView',
-    DetailView: 'common.widgets.DetailView',
-  },
-} as ModuleDescriptor;
+export default { name: MODULE_NAME, views } as ModuleDescriptor;
